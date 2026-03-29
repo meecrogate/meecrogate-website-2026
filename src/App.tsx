d20c3661b4bfea54eecea0b2ecbdc84829f73a9e
thomrg <thomrg@gmail.com>
2026-03-29 14:08:23 +0200

    add basename at BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/features" element={<FeaturesPage />} />

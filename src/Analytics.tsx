afa712565b23507483a32dfbbe6a187a6297cd4b
thomrg <thomrg@gmail.com>
2026-04-28 10:58:20 +0200

    add seo fixed

--- src/Analytics.tsx
+++ src/Analytics.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import { useMatomo } from '@datapunt/matomo-tracker-react';
+import { useMatomo } from '@jonkoops/matomo-tracker-react';
 
 export const MatomoTracker = () => {
   const location = useLocation();
